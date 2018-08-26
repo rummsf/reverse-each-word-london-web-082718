e32b43cec1d097bb3605c3cc142d5b775952b72e
Irum F <irummf@gmail.com>
2018-08-26 17:51:25 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(sentence)
-  sentence = [] .split(//)
-  words = []
-  sentence.collect do |word|
+  words = sentence.split(//)
+  words.collect do |word|
     words.reverse 
 end 
 words
